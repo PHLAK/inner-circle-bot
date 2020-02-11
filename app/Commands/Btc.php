dbfa90185cd303417acf18dffb76c4ccf3d9c323
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-02-11 13:20:25 -0700

    Return a random sticker when btc date is in the future

--- app/Commands/Btc.php
+++ app/Commands/Btc.php
@@ -7,9 +7,24 @@ use BotMan\BotMan\BotMan;
 use Carbon\Carbon;
 use GuzzleHttp\Exception\ClientException;
 use NumberFormatter;
+use Tightenco\Collect\Support\Collection;
 
 class Btc
 {
+    /** @const Array of sticker IDs */
+    protected const STICKER_IDS = [
+        'CAACAgIAAxkBAAIl5F5C_iECLpOpC7_O_FZai2Ki9_sEAAIrBgAClvoSBWfibcvyr59BGAQ',
+        'CAACAgIAAxkBAAIl615C_tsPcny0eJxr9i7SG6o3BeDrAAIUBgAClvoSBRQaZ_yiYCJvGAQ',
+        'CAACAgIAAxkBAAIl7V5C_uoYJ5l91n8WNMEBSFov46DKAAL8BQAClvoSBfP45oIb_tb9GAQ',
+        'CAACAgIAAxkBAAIl715C_vjocN4BQu86UT1r49vxdnwzAAIIBgAClvoSBcJQcBHeJ36WGAQ',
+        'CAACAgIAAxkBAAIl8V5C_wg_D42ZAULQB_IbM6CK_T8-AAIkBgAClvoSBdvpo6DwpLO-GAQ',
+        'CAACAgIAAxkBAAIl815C_xTjj9LYupGrPsZNFZnPbiqAAAImBgAClvoSBT54uAwflqC1GAQ',
+        'CAACAgIAAxkBAAIl9V5C_xs7xP3cHnmp3ImR1dklc9C_AAL5BQAClvoSBWGMTeRYo89hGAQ',
+        'CAACAgIAAxkBAAIl915C_yZalVj1b7YNPteAaJxJMCMxAAIjBgAClvoSBRG5NgABoPOWfBgE',
+        'CAACAgIAAxkBAAIl-V5C_zO1VBU1TT9asHdmORgL7FCNAAIhBgAClvoSBZR14Lgl_SMkGAQ',
+        'CAACAgIAAxkBAAIl-15C_0Qz1iKGBKzAHv2hJClW7u7OAAMGAAKW-hIFYZOr_0h87woYBA',
+    ];
+
     /**
      * Handle the incoming request.
      *
@@ -21,6 +36,14 @@ class Btc
      */
     public function __invoke(BotMan $botman, string $date = null, ?CoinbaseClient $coinbase = null)
     {
+        if (Carbon::parse($date)->isFuture()) {
+            $botman->sendRequest('sendSticker', [
+                'sticker' => Collection::make(self::STICKER_IDS)->random()
+            ]);
+
+            return;
+        }
+
         $coinbase = $coinbase ?? new CoinbaseClient();
 
         try {
--- tests/Commands/BtcTest.php
+++ tests/Commands/BtcTest.php
@@ -12,6 +12,20 @@ use Tests\TestCase;
 
 class BtcTest extends TestCase
 {
+    /** @const Array of sticker IDs */
+    protected const STICKER_IDS = [
+        'CAACAgIAAxkBAAIl5F5C_iECLpOpC7_O_FZai2Ki9_sEAAIrBgAClvoSBWfibcvyr59BGAQ',
+        'CAACAgIAAxkBAAIl615C_tsPcny0eJxr9i7SG6o3BeDrAAIUBgAClvoSBRQaZ_yiYCJvGAQ',
+        'CAACAgIAAxkBAAIl7V5C_uoYJ5l91n8WNMEBSFov46DKAAL8BQAClvoSBfP45oIb_tb9GAQ',
+        'CAACAgIAAxkBAAIl715C_vjocN4BQu86UT1r49vxdnwzAAIIBgAClvoSBcJQcBHeJ36WGAQ',
+        'CAACAgIAAxkBAAIl8V5C_wg_D42ZAULQB_IbM6CK_T8-AAIkBgAClvoSBdvpo6DwpLO-GAQ',
+        'CAACAgIAAxkBAAIl815C_xTjj9LYupGrPsZNFZnPbiqAAAImBgAClvoSBT54uAwflqC1GAQ',
+        'CAACAgIAAxkBAAIl9V5C_xs7xP3cHnmp3ImR1dklc9C_AAL5BQAClvoSBWGMTeRYo89hGAQ',
+        'CAACAgIAAxkBAAIl915C_yZalVj1b7YNPteAaJxJMCMxAAIjBgAClvoSBRG5NgABoPOWfBgE',
+        'CAACAgIAAxkBAAIl-V5C_zO1VBU1TT9asHdmORgL7FCNAAIhBgAClvoSBZR14Lgl_SMkGAQ',
+        'CAACAgIAAxkBAAIl-15C_0Qz1iKGBKzAHv2hJClW7u7OAAMGAAKW-hIFYZOr_0h87woYBA',
+    ];
+
     public function test_it_can_get_the_current_price(): void
     {
         $botman = $this->createMock(BotMan::class);
@@ -38,14 +52,23 @@ class BtcTest extends TestCase
 
         $coinbase = $this->createMock(CoinbaseClient::class);
         $coinbase->expects($this->once())->method('priceOn')->willReturn(
-            (object) ['data' => (object) [
-                'base' => 'BTC',
-                'currency' => 'USD',
-                'amount' => 1337.42
-            ]]
+            json_decode('{"data": {"base": "BTC", "currency": "USD", "amount": 1337.42}}')
         );
 
         (new Btc)($botman, '1986-05-20', $coinbase);
+    }
+
+    public function test_it_can_get_the_future_price(): void
+    {
+        $botman = $this->createMock(BotMan::class);
+        $botman->expects($this->once())->method('sendRequest')->with(
+            'sendSticker',
+            $this->callback(function (array $parameter): bool {
+                return in_array($parameter['sticker'], self::STICKER_IDS);
+            })
+        );
+
+        (new Btc)($botman, '2986-05-20', $this->createMock(CoinbaseClient::class));
     }
 
     public function test_it_returns_an_error_message_when_it_fails_to_fetch_the_price(): void
