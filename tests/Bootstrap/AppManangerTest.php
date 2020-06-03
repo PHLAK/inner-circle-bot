574dcf987de0812f10280fa9e15f146d677a332f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-06-03 09:46:33 -0700

    Minor test fix

--- tests/Bootstrap/AppManangerTest.php
+++ tests/Bootstrap/AppManangerTest.php
@@ -10,7 +10,7 @@ class AppManangerTest extends TestCase
 {
     public function test_it_returns_an_app_instance()
     {
-        $app = (new AppManager($this->container))($this->path());
+        $app = (new AppManager($this->container))();
 
         $this->assertInstanceOf(App::class, $app);
         $this->assertSame($this->container, $app->getContainer());
